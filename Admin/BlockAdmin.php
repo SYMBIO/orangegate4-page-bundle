46e558702d280455a46a8a17b8f675c638f13a8f
Sklico <michal.sklenar@Administrators-MacBook-Pro-2.local>
2015-06-10 14:38:56 +0200

    vlastny compose_container_show.html.twig, pridany js namiesto composer.js, uprava BlockAdmin, uprava compose.html.twig

--- Admin/BlockAdmin.php
+++ Admin/BlockAdmin.php
@@ -3,7 +3,7 @@
 namespace Symbio\OrangeGate\PageBundle\Admin;
 
 use Doctrine\ORM\EntityRepository;
-use Sonata\PageBundle\Admin\BaseBlockAdmin as BaseAdmin;
+use Sonata\PageBundle\Admin\BlockAdmin as BaseAdmin;
 use Sonata\AdminBundle\Route\RouteCollection;
 use Sonata\PageBundle\Model\PageInterface;
 use Sonata\AdminBundle\Form\FormMapper;
@@ -43,88 +43,6 @@ class BlockAdmin extends BaseAdmin
             ->add('updatedAt')
             ->add('position')
         ;
-    }
-
-
-    /**
-     * {@inheritdoc}
-     */
-    protected function configureFormFields(FormMapper $formMapper)
-    {
-        $block = $this->getSubject();
-
-        $page = false;
-
-        if ($this->getParent()) {
-            $page = $this->getParent()->getSubject();
-
-            if (!$page instanceof PageInterface) {
-                throw new \RuntimeException('The BlockAdmin must be attached to a parent PageAdmin');
-            }
-
-            if ($block->getId() === null) { // new block
-                $block->setType($this->request->get('type'));
-                $block->setPage($page);
-            }
-
-            if ($block->getPage()->getId() != $page->getId()) {
-                throw new \RuntimeException('The page reference on BlockAdmin and parent admin are not the same');
-            }
-        }
-
-        $isContainerRoot = $block && in_array($block->getType(), array('sonata.page.block.container', 'sonata.block.service.container')) && !$this->hasParentFieldDescription();
-        $isStandardBlock = $block && !in_array($block->getType(), array('sonata.page.block.container', 'sonata.block.service.container')) && !$this->hasParentFieldDescription();
-
-        if ($isContainerRoot || $isStandardBlock) {
-            $service = $this->blockManager->get($block);
-
-            $containerBlockTypes = $this->containerBlockTypes;
-
-            //$formMapper->with($this->trans('form.field_group_options'));
-
-            // need to investigate on this case where $page == null ... this should not be possible
-            if ($isStandardBlock && $page && !empty($containerBlockTypes)) {
-                $formMapper->add('parent', 'entity', array(
-                    'class' => $this->getClass(),
-                    'query_builder' => function(EntityRepository $repository) use ($page, $containerBlockTypes) {
-                        return $repository->createQueryBuilder('a')
-                            ->andWhere('a.page = :page AND a.type IN (:types)')
-                            ->setParameters(array(
-                                'page'  => $page,
-                                'types' => $containerBlockTypes,
-                            ));
-                    }
-                ),array(
-                    'admin_code' => $this->getCode()
-                ));
-            }
-
-            if ($isStandardBlock) {
-                $formMapper->add('position', 'integer');
-            }
-
-            //$formMapper->add('name');
-
-            if ($block->getId() > 0) {
-                $service->buildEditForm($formMapper, $block);
-            } else {
-                $service->buildCreateForm($formMapper, $block);
-            }
-
-        } else {
-
-            //$formMapper->with($this->trans('form.field_group_general'));
-
-            // add name on all forms
-            $formMapper->add('name');
-
-            $formMapper
-                ->add('type', 'sonata_block_service_choice', array(
-                    'context' => 'sonata_page_bundle'
-                ))
-                //->add('enabled')
-                ->add('position', 'integer');
-        }
     }
 
     /**
