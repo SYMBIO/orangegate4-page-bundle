b56ebb62b9d458669f9100e121fb176eff7874e6
Sklico <michal.sklenar@Sklico.local>
2015-07-28 17:16:43 +0200

    exchange of conditions

--- CmsManager/CmsPageManager.php
+++ CmsManager/CmsPageManager.php
@@ -41,10 +41,10 @@ class CmsPageManager extends BaseCmsPageManager
     {
         if (is_string($page) && substr($page, 0, 1) == '/') {
             $page = $this->getPageByUrl($site, $page);
-        } elseif (is_string($page)) { // page is a slug, load the related page
-            $page = $this->getPageByRouteName($site, $page);
         } elseif (is_numeric($page)) {
             $page = $this->getPageById($page);
+        } elseif (is_string($page)) { // page is a slug, load the related page
+            $page = $this->getPageByRouteName($site, $page);
         } elseif (!$page) { // get the current page
             $page = $this->getCurrentPage();
         }
