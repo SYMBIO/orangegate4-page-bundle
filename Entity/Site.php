3505e0996735417b2f4ddac1ef02dfe789769be2
Martin Cajthaml <martin.cajthaml@symbiodigital.com>
2015-10-29 02:45:27 +0100

    Fixed relation of a site to languageversions

--- Entity/Site.php
+++ Entity/Site.php
@@ -22,7 +22,7 @@ class Site extends BaseSite
     protected $id;
 
     /**
-     * @ORM\OneToMany(targetEntity="LanguageVersion", mappedBy="site", cascade={"persist", "remove"})
+     * @ORM\OneToMany(targetEntity="LanguageVersion", mappedBy="site", indexBy="locale", cascade={"all"}, orphanRemoval=true)
      */
     protected $languageVersions;
 
