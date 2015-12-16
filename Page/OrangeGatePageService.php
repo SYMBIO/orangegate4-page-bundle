e1cfbe79a9d172e2efd1d0e5bf3b04300fa6b0e2
Tomáš Vítek <tomas.vitek@symbio.cz>
2015-12-16 14:54:46 +0100

    Use HTTP_HOST as og:url if site host is set to 'localhost'

--- Page/OrangeGatePageService.php
+++ Page/OrangeGatePageService.php
@@ -99,7 +99,12 @@ class OrangeGatePageService extends BasePageService
         }
 
         $this->seoPage->addMeta('property', 'og:site_name', $languageVersion->getTitle());
-        $this->seoPage->addMeta('property', 'og:url', 'http://'.$languageVersion->getHost().$languageVersion->getRelativePath());
+        if (($languageVersion->getHost() && $languageVersion->getHost() != 'localhost') || isset($_SERVER['HTTP_HOST'])) {
+            $protocol = ((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off') || $_SERVER['SERVER_PORT'] == 443) ? 'https' : 'http';
+            $host = $languageVersion->getHost() && $languageVersion->getHost() != 'localhost' ? $languageVersion->getHost() : $_SERVER['HTTP_HOST'];
+            $this->seoPage->addMeta('property', 'og:url', $protocol . '://' . $host . $languageVersion->getRelativePath());
+        }
+
         $this->seoPage->addMeta('property', 'og:type', 'website');
         $this->seoPage->addHtmlAttributes('prefix', 'og: http://ogp.me/ns#');
     }
